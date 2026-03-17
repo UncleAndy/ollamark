fabe66e22d1f17788435075e5548cb5514bb7822
Andrey Velikoredchanin <andy@andyhost.ru>
2026-03-17 07:06:53 +0100

    Delete "Share Benchmark" button

--- main.go
+++ main.go
@@ -8,23 +8,13 @@ package main
 import (
 	"bytes"
 	"context"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
-	"encoding/pem"
 	"flag"
 	"fmt"
 	"image/color"
 	"io"
+	"log/slog"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
 	"runtime"
@@ -42,28 +32,9 @@ import (
 	"fyne.io/fyne/v2/widget"
 	xwidget "fyne.io/x/fyne/widget"
 	"github.com/context-labs/ollamark/v2/internal/assets"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/shirou/gopsutil/mem"
 )
-
-type BenchmarkResult struct {
-	ModelName       string              `json:"model_name"`
-	Timestamp       int64               `json:"timestamp"`
-	Duration        float64             `json:"duration"`
-	TokensPerSecond float64             `json:"tokens_per_second"`
-	EvalCount       int                 `json:"eval_count"`
-	EvalDuration    int64               `json:"eval_duration"`
-	Iterations      int                 `json:"iterations"`
-	SysInfo         *SysInfo            `json:"sys_info"`
-	GPUInfo         *GPUInfo            `json:"gpu_info"`
-	OllamaVersion   string              `json:"ollama_version"`
-	ClientType      string              `json:"client_type"`
-	ClientVersion   string              `json:"client_version"`
-	IP              string              `json:"ip"`
-	ProofOfWork     ProofOfWorkSolution `json:"proof_of_work"`
-}
 
 type OllamaRequest struct {
 	ModelName string `json:"model"`
@@ -109,53 +80,6 @@ var (
 )
 
 // ProofOfWorkChallenge represents a proof-of-work challenge
-type ProofOfWorkChallenge struct {
-	Challenge  string `json:"challenge"`
-	Difficulty int    `json:"difficulty"`
-	Timestamp  int64  `json:"timestamp"`
-}
-
-// ProofOfWorkSolution represents a solution to a proof-of-work challenge
-type ProofOfWorkSolution struct {
-	Challenge  string `json:"challenge"`
-	Nonce      string `json:"nonce"`
-	Timestamp  int64  `json:"timestamp"`
-	Difficulty int    `json:"difficulty"`
-}
-
-// requestProofOfWorkChallenge requests a new proof-of-work challenge from the server
-func requestProofOfWorkChallenge(apiEndpoint string) (ProofOfWorkChallenge, error) {
-	resp, err := http.Get(apiEndpoint + "/api/pow-challenge")
-	if err != nil {
-		return ProofOfWorkChallenge{}, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return ProofOfWorkChallenge{}, err
-	}
-
-	var challenge ProofOfWorkChallenge
-	if err := json.Unmarshal(body, &challenge); err != nil {
-		return ProofOfWorkChallenge{}, err
-	}
-
-	return challenge, nil
-}
-
-// solveProofOfWork solves the proof-of-work challenge
-func solveProofOfWork(challenge ProofOfWorkChallenge) (string, error) {
-	prefix := strings.Repeat("0", challenge.Difficulty)
-	for i := 0; ; i++ {
-		nonce := strconv.Itoa(i)
-		hash := sha256.Sum256([]byte(challenge.Challenge + nonce))
-		if strings.HasPrefix(hex.EncodeToString(hash[:]), prefix) {
-			return nonce, nil
-		}
-	}
-}
-
 type ModelInfo struct {
 	Name         string `json:"name"`
 	Parameters   string `json:"parameters"`
@@ -202,78 +126,6 @@ func initModels() error {
 	return nil
 }
 
-func LoadPublicKey() (*rsa.PublicKey, error) {
-	publicKeyData := os.Getenv("PUBLIC_KEY")
-	block, _ := pem.Decode([]byte(publicKeyData))
-	if block == nil {
-		return nil, fmt.Errorf("failed to parse PEM block containing the public key")
-	}
-
-	publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	rsaPublicKey, ok := publicKey.(*rsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("failed to cast public key to RSA public key")
-	}
-
-	return rsaPublicKey, nil
-}
-
-func EncryptData(publicKey *rsa.PublicKey, data []byte) ([]byte, error) {
-	return rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, data, nil)
-}
-
-// Generate a random AES key
-func generateAESKey() ([]byte, error) {
-	key := make([]byte, 32) // AES-256
-	_, err := rand.Read(key)
-	if err != nil {
-		return nil, err
-	}
-	return key, nil
-}
-
-// Encrypt data using AES-GCM
-func encryptAESGCM(key, plaintext []byte) ([]byte, []byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	nonce := make([]byte, aesGCM.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, nil, err
-	}
-
-	ciphertext := aesGCM.Seal(nil, nonce, plaintext, nil)
-	return nonce, ciphertext, nil
-}
-
-// Encrypt the AES key using RSA
-func encryptRSA(publicKey *rsa.PublicKey, data []byte) ([]byte, error) {
-	return rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, data, nil)
-}
-
-// Generate a random UUID
-func generateUUID() string {
-	return uuid.New().String()
-}
-
-// Sign the UUID with HMAC-SHA256
-func signUUID(uuid string, secretKey string) string {
-	h := hmac.New(sha256.New, []byte(secretKey))
-	h.Write([]byte(uuid))
-	return base64.StdEncoding.EncodeToString(h.Sum(nil))
-}
-
 func getCPUName() string {
 	// get windows cpu name
 	if runtime.GOOS == "windows" {
@@ -292,9 +144,14 @@ func getCPUName() string {
 	// get linux cpu name
 	if runtime.GOOS == "linux" {
 		cmd := exec.Command("lshw", "-C", "cpu")
+		cmd.Env = append(os.Environ(),
+			"LC_ALL=C",
+			"LANG=C",
+		)
 		output, err := cmd.Output()
 		if err != nil {
-			return "Unknown"
+			slog.Error("failed to parse CPU version: %v", err)
+			// return "Unknown"
 		}
 		lines := strings.Split(string(output), "\n")
 		for _, line := range lines {
@@ -545,19 +402,6 @@ func getAMDGPUInfoLinux() (*GPUInfo, error) {
 	return nil, fmt.Errorf("no AMD GPU detected")
 }
 
-func getIPAddress() string {
-	resp, err := http.Get("https://icanhazip.com")
-	if err != nil {
-		return "Unknown"
-	}
-	defer resp.Body.Close()
-	ip, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "Unknown"
-	}
-	return strings.TrimSpace(string(ip))
-}
-
 func getOllamaVersion() string {
 	mainURL := os.Getenv("OLLAMA_API")
 	if mainURL == "" {
@@ -633,8 +477,7 @@ func main() {
 		fmt.Println("  For Ollamark CLI mode:")
 		fmt.Println("      ollamark -m llama3 -i 10")
 		fmt.Println("      ollamark -m phi3")
-		fmt.Println("      ollamark -m phi3 -s")
-		fmt.Println("      ollamark -m phi3 -s -o " + defaultAPIHost + "/api/generate")
+		fmt.Println("      ollamark -m phi3 -o " + defaultAPIHost + "/api/generate")
 	}
 
 	// Parse command-line arguments (Ollamark CLI)
@@ -643,7 +486,6 @@ func main() {
 		defaultOllamaAPI = defaultAPIHost
 	}
 	modelPtr := flag.String("m", "llama3", "Model name to benchmark (default: llama3)")
-	submitPtr := flag.Bool("s", false, "Submit benchmark results to Ollamark.com (default false)")
 	ollamaPtr := flag.String("o", defaultOllamaAPI, "Ollama API endpoint (default "+defaultOllamaAPI+")")
 	iterationsPtr := flag.Int("i", 2, "Number of benchmark iterations (Min 2, Max 20)")
 	flag.Parse()
@@ -670,7 +512,7 @@ func main() {
 		}
 
 		// Run ollamark in CLI mode
-		runBenchmarkCLI(*modelPtr, *submitPtr, apiEndpoint, *iterationsPtr)
+		runBenchmarkCLI(*modelPtr, apiEndpoint, *iterationsPtr)
 		return
 	}
 
@@ -806,10 +648,6 @@ func main() {
 		gif.Show()
 	}
 
-	var benchmarkResult *BenchmarkResult
-	var submitButton *widget.Button
-	var linkButton *widget.Button
-
 	var benchmarkCancel context.CancelFunc
 	stopButton := widget.NewButton("Stop", nil)
 	stopButton.Disable()
@@ -822,10 +660,8 @@ func main() {
 
 	benchmarkButton := widget.NewButton("Benchmark", nil)
 	benchmarkButton.OnTapped = func() {
-		linkButton.Hide()
 		benchmarkButton.SetText("Benchmarking...")
 		benchmarkButton.Disable()
-		submitButton.Disable()
 		stopButton.Enable()
 
 		resultLabel.Show()
@@ -918,10 +754,6 @@ func main() {
 			resultLabel.Refresh()
 
 			var totalTokensPerSecond float64
-			var evalCount int
-			var evalDuration float64
-
-			start := time.Now()
 
 			for i := 0; i < iterations; i++ {
 				if ctx.Err() != nil {
@@ -1006,8 +838,6 @@ func main() {
 				tokensPerSecond := float64(response.EvalCount) / (float64(response.EvalDuration) / 1e9)
 
 				totalTokensPerSecond += tokensPerSecond
-				evalCount = response.EvalCount
-				evalDuration = float64(response.EvalDuration) / 1e9
 			}
 
 			if ctx.Err() != nil {
@@ -1019,26 +849,7 @@ func main() {
 				return
 			}
 
-			EvalCount := evalCount
-			EvalDuration := evalDuration
-
 			avgTokensPerSecond := totalTokensPerSecond / float64(iterations)
-
-			benchmarkResult = &BenchmarkResult{
-				ModelName:       modelName,
-				Timestamp:       time.Now().Unix(),
-				Duration:        time.Since(start).Seconds(),
-				EvalCount:       EvalCount,
-				EvalDuration:    int64(EvalDuration),
-				TokensPerSecond: avgTokensPerSecond,
-				Iterations:      iterations,
-				SysInfo:         sysinfo,
-				GPUInfo:         gpuinfo,
-				OllamaVersion:   ollamaVersion,
-				ClientType:      "ollamark-gui",
-				ClientVersion:   clientVersion,
-				IP:              getIPAddress(),
-			}
 
 			resultLabel.SetText(fmt.Sprintf("Benchmark completed for %s\nAverage Tokens per second: %.2f\nBenchmarked with %d iterations", modelName, avgTokensPerSecond, iterations))
 			resultLabel.Alignment = fyne.TextAlignCenter
@@ -1057,132 +868,8 @@ func main() {
 			progressBar.Refresh() // Refresh after hiding the ProgressBar
 			benchmarkButton.SetText("Benchmark")
 			benchmarkButton.Enable()
-			submitButton.Show()
-			submitButton.Enable()
 		}()
 	}
-
-	submitButton = widget.NewButton("Share Benchmark", nil)
-	linkButton = widget.NewButton("View on Ollamark.com", nil)
-	linkButton.Hide()
-
-	submitButton.OnTapped = func() {
-		if benchmarkResult != nil {
-			subEndpoint := os.Getenv("OLLAMARK_API")
-			secretKey := os.Getenv("KEY")
-			publicKey, err := LoadPublicKey()
-			if err != nil {
-				resultLabel.SetText("Error loading public key: " + err.Error())
-				return
-			}
-
-			// Generate AES key
-			aesKey, err := generateAESKey()
-			if err != nil {
-				resultLabel.SetText("Error generating AES key: " + err.Error())
-				return
-			}
-
-			var submissionID = generateUUID()
-
-			// Generate JWT token
-			jwtToken, err := generateJWT(submissionID)
-			if err != nil {
-				resultLabel.SetText("Error generating JWT token: " + err.Error())
-				return
-			}
-
-			// Request proof-of-work challenge
-			challenge, err := requestProofOfWorkChallenge(subEndpoint)
-			if err != nil {
-				resultLabel.SetText("Error requesting proof-of-work challenge: " + err.Error())
-				return
-			}
-
-			// Solve proof-of-work challenge
-			powNonce, err := solveProofOfWork(challenge)
-			if err != nil {
-				resultLabel.SetText("Error solving proof-of-work challenge: " + err.Error())
-				return
-			}
-
-			// Include proof-of-work solution in the benchmark result
-			benchmarkResult.ProofOfWork = ProofOfWorkSolution{
-				Challenge:  challenge.Challenge,
-				Nonce:      powNonce,
-				Timestamp:  challenge.Timestamp,
-				Difficulty: challenge.Difficulty,
-			}
-
-			// Encrypt benchmark result with AES key
-			jsonData, _ := json.Marshal(benchmarkResult)
-			nonce, encryptedData, err := encryptAESGCM(aesKey, jsonData)
-			if err != nil {
-				resultLabel.SetText("Error encrypting data with AES: " + err.Error())
-				return
-			}
-
-			// Encrypt AES key with RSA public key
-			encryptedAESKey, err := encryptRSA(publicKey, aesKey)
-			if err != nil {
-				resultLabel.SetText("Error encrypting AES key: " + err.Error())
-				return
-			}
-
-			// Prepare payload
-			payload := map[string]interface{}{
-				"data":          base64.StdEncoding.EncodeToString(encryptedData),
-				"nonce":         base64.StdEncoding.EncodeToString(nonce),
-				"encrypted_key": base64.StdEncoding.EncodeToString(encryptedAESKey),
-			}
-
-			payloadBytes, _ := json.Marshal(payload)
-
-			// Sign the UUID
-			signature := signUUID(submissionID, secretKey)
-
-			// Create and send the request
-			req, err := http.NewRequest("POST", subEndpoint+"/api/submit-benchmark", bytes.NewBuffer(payloadBytes))
-			if err != nil {
-				resultLabel.SetText("Error submitting benchmark! Try again!")
-				return
-			}
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Authorization", "Bearer "+jwtToken)
-			req.Header.Set("X-Submission-ID", submissionID)
-			req.Header.Set("X-Signature", signature)
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				resultLabel.SetText("Error submitting benchmark: " + err.Error())
-				return
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				body, _ := io.ReadAll(resp.Body)
-				resultLabel.SetText("Error submitting benchmark: " + string(body))
-				return
-			}
-
-			resultLabel.SetText("Benchmark submitted successfully!")
-			submitButton.Hide()
-			// set linkButton link
-			linkButton.OnTapped = func() {
-				submissionURL, err := url.Parse(fmt.Sprintf("https://ollamark.com/marks/%s", submissionID))
-				if err != nil {
-					fmt.Printf("Failed to parse URL: %v\n", err)
-					return
-				}
-				fyne.CurrentApp().OpenURL(submissionURL)
-			}
-			linkButton.Show()
-		}
-	}
-
-	submitButton.Hide()
-	linkButton.Hide()
 
 	// border/group around systext and gputext
 	sysInfoGroup := container.NewVBox(ollamaVersionText, sysText, gpuText)
@@ -1208,8 +895,6 @@ func main() {
 		// widget.NewSeparator(),
 		benchmarkButton,
 		stopButton,
-		submitButton,
-		linkButton,
 	)
 
 	// Wrap the content with a padded container
@@ -1228,12 +913,10 @@ func contains(models []ModelInfo, modelName string) bool {
 	return false
 }
 
-func runBenchmarkCLI(modelName string, submit bool, ollamaAPI string, iterations int) {
+func runBenchmarkCLI(modelName string, ollamaAPI string, iterations int) {
 	ollamaAPIURL := ollamaAPI
 
 	var totalTokensPerSecond float64
-	var evalCount int
-	var evalDuration float64
 
 	// modelName needs to match a model name in MODELS
 	if !contains(globalModels, modelName) {
@@ -1281,7 +964,6 @@ func runBenchmarkCLI(modelName string, submit bool, ollamaAPI string, iterations
 
 	fmt.Println("Model pulled successfully")
 	fmt.Println("Benchmarking...")
-	start := time.Now()
 
 	for i := 0; i < iterations; i++ {
 		requestBody := OllamaRequest{
@@ -1337,149 +1019,10 @@ func runBenchmarkCLI(modelName string, submit bool, ollamaAPI string, iterations
 		tokensPerSecond := float64(response.EvalCount) / (float64(response.EvalDuration) / 1e9)
 
 		totalTokensPerSecond += tokensPerSecond
-		evalCount = response.EvalCount
-		evalDuration = float64(response.EvalDuration) / 1e9
-
 	}
 
-	EvalCount := evalCount
-	EvalDuration := evalDuration
 	avgTokensPerSecond := totalTokensPerSecond / float64(iterations)
 
 	fmt.Printf("\nBenchmark completed for %s\n", modelName)
 	fmt.Printf("Average Tokens per second: %.2f\n", avgTokensPerSecond)
-
-	sysinfo, _ = getSysInfo()
-	gpuinfo, _ = getGPUInfo()
-
-	benchmarkResult := &BenchmarkResult{
-		ModelName:       modelName,
-		Timestamp:       time.Now().Unix(),
-		Duration:        time.Since(start).Seconds(),
-		EvalCount:       EvalCount,
-		EvalDuration:    int64(EvalDuration),
-		TokensPerSecond: avgTokensPerSecond,
-		Iterations:      iterations,
-		SysInfo:         sysinfo,
-		GPUInfo:         gpuinfo,
-		OllamaVersion:   getOllamaVersion(),
-		ClientType:      "ollamark-cli",
-		ClientVersion:   clientVersion,
-		IP:              getIPAddress(),
-	}
-
-	if submit {
-		submitBenchmark(benchmarkResult)
-	} else {
-		fmt.Println("Benchmark results not submitted.")
-	}
-}
-
-func generateJWT(nonce string) (string, error) {
-	secretKey := os.Getenv("KEY")
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iat":   time.Now().Unix(),
-		"exp":   time.Now().Add(time.Minute * 1).Unix(), // Token expires in 1 minute
-		"nonce": nonce,
-	})
-
-	tokenString, err := token.SignedString([]byte(secretKey))
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
-}
-
-func submitBenchmark(benchmarkResult *BenchmarkResult) error {
-	apiEndpoint := os.Getenv("OLLAMARK_API")
-	secretKey := os.Getenv("KEY")
-	publicKey, err := LoadPublicKey()
-	if err != nil {
-		return fmt.Errorf("error loading public key: %v", err)
-	}
-
-	// Generate AES key
-	aesKey, err := generateAESKey()
-	if err != nil {
-		return fmt.Errorf("error generating AES key: %v", err)
-	}
-
-	var submissionID = generateUUID()
-
-	// Generate JWT token
-	jwtToken, err := generateJWT(submissionID)
-	if err != nil {
-		return fmt.Errorf("error generating JWT token: %v", err)
-	}
-
-	// Request proof-of-work challenge
-	challenge, err := requestProofOfWorkChallenge(apiEndpoint)
-	if err != nil {
-		return fmt.Errorf("error requesting proof-of-work challenge: %v", err)
-	}
-
-	// Solve proof-of-work challenge
-	powNonce, err := solveProofOfWork(challenge)
-	if err != nil {
-		return fmt.Errorf("error solving proof-of-work challenge: %v", err)
-	}
-
-	// Include proof-of-work solution in the benchmark result
-	benchmarkResult.ProofOfWork = ProofOfWorkSolution{
-		Challenge:  challenge.Challenge,
-		Nonce:      powNonce,
-		Timestamp:  challenge.Timestamp,
-		Difficulty: challenge.Difficulty,
-	}
-
-	// Encrypt benchmark result with AES key
-	jsonData, _ := json.Marshal(benchmarkResult)
-	nonce, encryptedData, err := encryptAESGCM(aesKey, jsonData)
-	if err != nil {
-		return fmt.Errorf("error encrypting data with AES: %v", err)
-	}
-
-	// Encrypt AES key with RSA public key
-	encryptedAESKey, err := encryptRSA(publicKey, aesKey)
-	if err != nil {
-		return fmt.Errorf("error encrypting AES key: %v", err)
-	}
-
-	// Prepare payload
-	payload := map[string]interface{}{
-		"data":          base64.StdEncoding.EncodeToString(encryptedData),
-		"nonce":         base64.StdEncoding.EncodeToString(nonce),
-		"encrypted_key": base64.StdEncoding.EncodeToString(encryptedAESKey),
-	}
-
-	payloadBytes, _ := json.Marshal(payload)
-
-	// Sign the UUID
-	signature := signUUID(submissionID, secretKey)
-
-	// Create and send the request
-	req, err := http.NewRequest("POST", apiEndpoint+"/api/submit-benchmark", bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		return fmt.Errorf("error submitting benchmark! %v", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+jwtToken)
-	req.Header.Set("X-Submission-ID", submissionID)
-	req.Header.Set("X-Signature", signature)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("error submitting benchmark: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("server responded with status %d: %s", resp.StatusCode, body)
-	}
-
-	fmt.Printf("Benchmark submitted successfully! View it at: https://ollamark.com/marks/%s\n", submissionID)
-	return nil
 }
